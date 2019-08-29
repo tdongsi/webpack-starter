45c7347f9aeb462835052e7f1576e0576598d988
tdongsi <dongsi.tuecuong@gmail.com>
2019-08-28 18:53:32 -0700

    bind() method and constructor function.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,31 @@
 import '../styles/index.scss';
 
-let app = (function() {
-    let carId = 123;
+let o = {
+    cardId: 123,
+    getId: function(prefix) {
+        return prefix + this.cardId.toString();
+    }
+};
 
-    let getId = function() {
-        return carId;
+let newObj = {cardId: 456};
+
+// Changing context to change "this" and assign
+let newFunc = o.getId.bind(newObj, ["ID: "]);
+console.log(newFunc());
+// Output: 456
+
+let getId_1 = (prefix, suffix) => prefix + 123 + suffix;
+let getId_2 = (prefix, suffix) => {
+    return prefix + 123 + suffix;
+};
+console.log(getId_1('ID: ', '!'));
+
+function Car(id) {
+    this.id = id;
+    this.getId = function() {
+        return this.id;
     };
+}
 
-    return {getId: getId};
-})();
-
-console.log(app.getId());
+let car = new Car(123);
+console.log(car.getId());
